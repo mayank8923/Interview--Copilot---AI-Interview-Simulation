1ea694a96a694a396cfd71c9bdb56bee1683a721
mayank8923 <mayanksharma2169@gmail.com>
2026-08-26 16:53:45 +0530

    fix: restore missing EnableMongoAuditing import

--- backend/src/main/java/com/interviewcopilot/InterviewCopilotApplication.java
+++ backend/src/main/java/com/interviewcopilot/InterviewCopilotApplication.java
@@ -2,6 +2,7 @@ package com.interviewcopilot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
